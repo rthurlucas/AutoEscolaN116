7c69764f457032670413cd15046cffb84786a2f0
rthurlucas <arthulucasx696@gmail.com>
2026-04-22 00:16:12 -0300

    feat: incrementing DTO class for canceling Schedules

---
+++ src/main/java/br/com/senain116/autoescolan116/domain/instrucao/DadosCancelamentoAgendamento.java
@@ -0,0 +1,7 @@
+package br.com.senain116.autoescolan116.domain.instrucao;
+
+public record DadosCancelamentoAgendamento (
+            Long idInstrucao,
+            MotivoCancelamento motivoCancelamento
+){
+}
