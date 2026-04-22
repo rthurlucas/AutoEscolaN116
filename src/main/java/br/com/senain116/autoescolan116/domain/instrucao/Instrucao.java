f54a27d39a27779edc3221b35a4b2648c724c984
rthurlucas <arthulucasx696@gmail.com>
2026-04-22 00:18:14 -0300

    refactor: correcting Aluno @joinColumn, Instructor

--- src/main/java/br/com/senain116/autoescolan116/domain/instrucao/Instrucao.java
+++ src/main/java/br/com/senain116/autoescolan116/domain/instrucao/Instrucao.java
@@ -4,6 +4,8 @@ import br.com.senain116.autoescolan116.domain.aluno.Aluno;
 import br.com.senain116.autoescolan116.domain.instrutor.Especialidade;
 import br.com.senain116.autoescolan116.domain.instrutor.Instrutor;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -21,17 +23,33 @@ public class Instrucao {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "aluno_id")
+    @JoinColumn(name = "alunos_id")
     private Aluno aluno;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "instrutor_id")
+    @JoinColumn(name = "instrutores_id")
     private Instrutor instrutor;
 
-    @JoinColumn(name = "data_hora")
+    @Column(name = "data_hora")
     private LocalDateTime datahora;
+    
+    private boolean ativo = true;
 
-    public void excluir(Instrucao instrucao){
-        this.excluir(instrucao);
+    @Enumerated
+    private MotivoCancelamento motivoCancelamento;
+
+    public Instrucao(Long id, Aluno aluno, Instrutor instrutor, LocalDateTime datahora) {
+        this.id = id;
+        this.aluno = aluno;
+        this.instrutor = instrutor;
+        this.datahora = datahora;
+        this.ativo = true;
     }
+
+    public Instrucao(Long aLong, MotivoCancelamento motivoCancelamento) {
+    }
+
+    public void excluir(){
+        this.ativo = false;
+   }
 }
