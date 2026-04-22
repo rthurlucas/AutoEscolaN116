2b58c29526710c26ca357a01c8e56313d0db636e
rthurlucas <arthulucasx696@gmail.com>
2026-04-22 00:16:53 -0300

    feat: classe Exception DadosMotivoCancelamento.java

---
+++ src/main/java/br/com/senain116/autoescolan116/domain/instrucao/validacao/DadosMotivoCancelamento.java
@@ -0,0 +1,7 @@
+package br.com.senain116.autoescolan116.domain.instrucao.validacao;
+
+public class DadosMotivoCancelamento extends RuntimeException {
+    public DadosMotivoCancelamento(String message) {
+        super(message);
+    }
+}
